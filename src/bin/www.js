3016c496dfcb158741465c8ae589a61140f0e041
mark <102784820+comet1019@users.noreply.github.com>
2023-10-04 12:57:26 +0900

    첫 커밋 및 db연동

--- src/bin/www.js
+++ src/bin/www.js
@@ -3,12 +3,8 @@
 const app = require("../../app");
 require('dotenv').config({path:"../../.env"})
 
-
-
 // 연결 포트 지정
 const port = process.env.PORT;
-
-
 
 app.listen(port, () => {
     console.log(`express server running on port ${port}`)
